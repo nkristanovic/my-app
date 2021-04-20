587bcce33e17cebab880873898dc0e7a8278ea53
Nikola Kristanović <nikola.kristanovic@gmail.com>
2021-04-20 21:18:52 +0200

    Added EventsCard to function

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,11 +1,122 @@
 import React from 'react';
+import Section from '../../components/Section/Section';
+import Grid from '../../components/Grid/Grid';
+import EventsCard from '../../components/EventsCard/EventsCard';
 
 function Events() {
   return (
     <>
-        
-    </>      
-        
+        <Section title="Events">
+          <Grid columns="4">
+          <EventsCard
+                        eventTitle1="UX/UI  workshop"
+                        itemTitle1="Lokacija"
+                        itemValue1="Hodnik FOI-a"
+                        
+                        itemTitle2="Lokacija"
+                        itemValue2="Hodnik FOI-a"
+                        
+                        itemTitle3="Lokacija"
+                        itemValue3="Hodnik FOI-a"
+                        
+                        itemTitle4="Lokacija"
+                        itemValue4="Hodnik FOI-a"
+                        buttonText="Find out more"
+                    />
+          <EventsCard
+                       eventTitle1="UX/UI  workshop"
+                       itemTitle1="Lokacija"
+                       itemValue1="Hodnik FOI-a"
+                       
+                       itemTitle2="Lokacija"
+                       itemValue2="Hodnik FOI-a"
+                       
+                       itemTitle3="Lokacija"
+                       itemValue3="Hodnik FOI-a"
+                       
+                       itemTitle4="Lokacija"
+                       itemValue4="Hodnik FOI-a"
+                       buttonText="Find out more"
+                    />
+          <EventsCard
+                      eventTitle1="UX/UI  workshop"
+                      itemTitle1="Lokacija"
+                      itemValue1="Hodnik FOI-a"
+                      
+                      itemTitle2="Lokacija"
+                      itemValue2="Hodnik FOI-a"
+                      
+                      itemTitle3="Lokacija"
+                      itemValue3="Hodnik FOI-a"
+                      
+                      itemTitle4="Lokacija"
+                      itemValue4="Hodnik FOI-a"
+                      buttonText="Find out more"
+                    />
+          <EventsCard
+                      eventTitle1="UX/UI  workshop"
+                      itemTitle1="Lokacija"
+                      itemValue1="Hodnik FOI-a"
+                      
+                      itemTitle2="Lokacija"
+                      itemValue2="Hodnik FOI-a"
+                      
+                      itemTitle3="Lokacija"
+                      itemValue3="Hodnik FOI-a"
+                      
+                      itemTitle4="Lokacija"
+                      itemValue4="Hodnik FOI-a"
+                      buttonText="Find out more"
+                    />
+          <EventsCard
+                      eventTitle1="UX/UI  workshop"
+                      itemTitle1="Lokacija"
+                      itemValue1="Hodnik FOI-a"
+                      
+                      itemTitle2="Lokacija"
+                      itemValue2="Hodnik FOI-a"
+                      
+                      itemTitle3="Lokacija"
+                      itemValue3="Hodnik FOI-a"
+                      
+                      itemTitle4="Lokacija"
+                      itemValue4="Hodnik FOI-a"
+                      buttonText="Find out more"
+                    />
+          <EventsCard
+                      eventTitle1="UX/UI  workshop"
+                      itemTitle1="Lokacija"
+                      itemValue1="Hodnik FOI-a"
+                      
+                      itemTitle2="Lokacija"
+                      itemValue2="Hodnik FOI-a"
+                      
+                      itemTitle3="Lokacija"
+                      itemValue3="Hodnik FOI-a"
+                      
+                      itemTitle4="Lokacija"
+                      itemValue4="Hodnik FOI-a"
+                      buttonText="Find out more"
+                    />
+          <EventsCard
+                      eventTitle1="UX/UI  workshop"
+                      itemTitle1="Lokacija"
+                      itemValue1="Hodnik FOI-a"
+                      
+                      itemTitle2="Lokacija"
+                      itemValue2="Hodnik FOI-a"
+                      
+                      itemTitle3="Lokacija"
+                      itemValue3="Hodnik FOI-a"
+                      
+                      itemTitle4="Lokacija"
+                      itemValue4="Hodnik FOI-a"
+                      buttonText="Find out more"
+                    />
+                  
+          </Grid>
+        </Section>
+    </>
   );
 }
 
