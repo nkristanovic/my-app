c957929249e6e110e738a2583a8dfad3be856d2c
Nikola Kristanović <nikola.kristanovic@gmail.com>
2021-04-27 17:44:08 +0200

    Added dynamic routing

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,79 +1,45 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import eventsMock from '../../lib/mock/events';
 import Section from '../../components/Section/Section';
 import { Grid } from '../../lib/style/generalStyles';
 import EventsCard from '../../components/EventsCard/EventsCard';
 
-const Events = () => {
+const Events = (props) => {
+
+  const routeEventId = parseInt(props.match.params.id);
+  const [events, setEvents] = useState(null);
+  const [event, setEvent] = useState(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setEvents(eventsMock);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    events && setEvent(...events.filter(event => event.id === routeEventId));
+  }, [events]);
+
   return (
     <>
-      <Section title="Events" withoutTopPadding={true}>
-        <Grid columns={4}>
-        <EventsCard
-          title="UX/UI workshop"
-          location="Hodnik FOI-a"
-          dateTime="14.10 (9:00-16:00h)"
-          availability="15/60"
-          company="Speck"
-          buttonText="Find out more"
-        />
-        <EventsCard
-          title="UX/UI workshop"
-          location="Hodnik FOI-a"
-          dateTime="14.10 (9:00-16:00h)"
-          availability="15/60"
-          company="Speck"
-          buttonText="Find out more"
-        />
-        <EventsCard
-          title="UX/UI workshop"
-          location="Hodnik FOI-a"
-          dateTime="14.10 (9:00-16:00h)"
-          availability="15/60"
-          company="Speck"
-          buttonText="Find out more"
-        />
-        <EventsCard
-          title="UX/UI workshop"
-          location="Hodnik FOI-a"
-          dateTime="14.10 (9:00-16:00h)"
-          availability="15/60"
-          company="Speck"
-          buttonText="Find out more"
-        />
-        <EventsCard
-          title="UX/UI workshop"
-          location="Hodnik FOI-a"
-          dateTime="14.10 (9:00-16:00h)"
-          availability="15/60"
-          company="Speck"
-          buttonText="Find out more"
-        />
-        <EventsCard
-          title="UX/UI workshop"
-          location="Hodnik FOI-a"
-          dateTime="14.10 (9:00-16:00h)"
-          availability="15/60"
-          company="Speck"
-          buttonText="Find out more"
-        />
-        <EventsCard
-          title="UX/UI workshop"
-          location="Hodnik FOI-a"
-          dateTime="14.10 (9:00-16:00h)"
-          availability="15/60"
-          company="Speck"
-          buttonText="Find out more"
-        />   
-        <EventsCard
-          title="UX/UI workshop"
-          location="Hodnik FOI-a"
-          dateTime="14.10 (9:00-16:00h)"
-          availability="15/60"
-          company="Speck"
-          buttonText="Find out more"
-        />         
-        </Grid>
-      </Section>
+      {events && <>
+        <Section title="Events" withoutTopPadding={true}>
+          <Grid columns={4}>
+          {events.map(event => (
+          <EventsCard
+            key={event.id}
+            route={`/event/${event.id}`}
+            title={event.title}
+            location={event.location}
+            dateTime={event.dateTime}
+            availability={event.availability}
+            company={event.company}
+            buttonText="Find out more"
+          />
+          ))}
+          </Grid>
+        </Section>
+      </>}
     </>
   );
 }
