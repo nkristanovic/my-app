89495d19c5eaf692e6474024910033af148949d4
Nikola Kristanović <nikola.kristanovic@gmail.com>
2021-04-29 17:28:45 +0200

    Added loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,22 @@
+import Loader from "react-loader-spinner";
+import styled from 'styled-components';
+
+const LoaderWrapper = styled.div `
+    text-align: center;
+`;
+
+const LoaderBars = () => {
+    return (
+        <LoaderWrapper>
+            <Loader 
+            type="Oval" 
+            color= "#333333"
+            height={80} 
+            width={80} 
+            timeout={1000}
+            />
+        </LoaderWrapper>
+    )
+}
+
+export default LoaderBars;
