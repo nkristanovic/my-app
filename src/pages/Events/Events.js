ce89b220b1739f1b4e4e190b2b62935da2aba4e8
Nikola Kristanović <nikola.kristanovic@gmail.com>
2021-04-30 00:30:18 +0200

    Imported SearchBar to events

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -4,27 +4,33 @@ import Section from '../../components/Section/Section';
 import { Grid } from '../../lib/style/generalStyles';
 import EventsCard from '../../components/EventsCard/EventsCard';
 import LoaderBars from '../../components/Loader/Loader';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 const Events = (props) => {
 
   const routeEventId = parseInt(props.match.params.id);
   const [events, setEvents] = useState(null);
   const [event, setEvent] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (loading === true) {
     setTimeout(() => {
+      setLoading(false);
       setEvents(eventsMock);
     }, 1000);
-  }, []);
+  } 
+}, [loading]);
 
   useEffect(() => {
     event && setEvent(...events.filter(event => event.id === routeEventId));
-  }, [events]);
+  }, [events, event, routeEventId]);
 
   return (
     <>
       <Section title="Events" withoutTopPadding={true}>
-      <LoaderBars />
+      <SearchBar />
+      {loading ? <LoaderBars /> : ''}
         {events && 
             <Grid columns={4}>
             {events.map(event => (
