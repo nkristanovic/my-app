6c34b7d6401fde5783a778f04e2700ccdd85d3fa
Nikola Kristanović <nikola.kristanovic@gmail.com>
2021-05-05 13:40:16 +0200

    Added admin js

---
+++ src/pages/Admin/Admin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Section from '../../components/Section/Section';
+import Table from '../../components/Table/Table';
+
+const Admin = () => {
+    return (
+        <>
+            <Section>
+                <Table />
+            </Section>
+        </>
+    )
+}
+
+export default Admin;
